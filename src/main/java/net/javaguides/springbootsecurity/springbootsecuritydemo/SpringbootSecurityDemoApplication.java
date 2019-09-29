98eaf41f9be1f9403b248dbffe9655706b9bbd40
LukaszP <lukaszp4@onet.eu>
2019-09-29 23:13:56 +0200

    Initial commit

---
+++ src/main/java/net/javaguides/springbootsecurity/springbootsecuritydemo/SpringbootSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springbootsecurity.springbootsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityDemoApplication.class, args);
+	}
+
+}
